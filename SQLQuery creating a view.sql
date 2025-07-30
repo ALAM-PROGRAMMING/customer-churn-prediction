f23b8f48529ce7c4fb5f5ceb7543c1bfb40f266d
mohammad muaz alam <mohammad.alam@s.amity.edu>
2025-07-30 19:04:11 +0530

    Add files via upload

---
+++ SQLQuery creating a view.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS vw_ChurnData;
+GO
+
+CREATE VIEW vw_ChurnData AS
+SELECT * FROM prod_Churn WHERE Customer_Status IN ('Churned', 'Stayed');
+GO
+
+DROP VIEW IF EXISTS vw_JoinData;
+GO
+
+CREATE VIEW vw_JoinData AS
+SELECT * FROM prod_Churn WHERE Customer_Status = 'Joined';
+GO
